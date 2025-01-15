b12db1786f090f040d7d646af7b1f9ce0913691c
vandana108 <thevandanarai108@gmail.com>
2025-01-15 22:39:18 +0530

    Basics of Javascript

---
+++ Javascript/Basics/index.js
@@ -0,0 +1,8 @@
+console.log('Hello Geeks!!');
+console.log("My name is Vandana Rai")
+console.log(7+3);
+
+var message;
+message = "Hello GeeksforGeeks!";
+message= 'My name is Prakash Sakari';
+console.log(message)
