6100d7605ed2c6e173f373ea0e659d2910d14d3f
vandana108 <thevandanarai108@gmail.com>
2025-01-17 13:00:38 +0530

    Comparison Operator

---
+++ Javascript/Comparison Operation.js
@@ -0,0 +1,33 @@
+// comparison Operation
+
+// console.log(30> 60);
+// console.log(30> 20);
+console.log(30>=30);
+console.log(50==40);
+
+
+//String
+ console.log("apple">"banana");
+ console.log("glow">"glowing")
+ console.log("bc"<"y"); 
+ console.log("01"==1); //there is no comparison between  these no. because both are different but still it would give true  result as output because string is convert into number then  it make comparison so for this case we have to programm it as given below
+ // console.log("05">=1);
+
+ //  strict equality
+
+ console.log("01"===1);
+
+//  
+
+
+console.log(null == undefined )
+console.log(null === undefined)
+
+console.log(null==0)// null is null here
+
+console.log("2">"3");
+console.log(undefined == null);
+console.log(null>1)
+console.log(null<1);
+
+
