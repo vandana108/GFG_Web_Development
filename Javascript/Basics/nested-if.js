d17981818b8021ffa0389a44b26afad800575d32
vandana108 <thevandanarai108@gmail.com>
2025-01-17 23:58:42 +0530

    Nested-if

---
+++ Javascript/Basics/nested-if.js
@@ -0,0 +1,18 @@
+const number = Number(readlineSync.question("enter a number"));
+const remainder= number%2;
+
+if(remainder===0)
+{
+  console.log(`${number} is an even number`);
+  if(number%4===0)
+  {
+    console.log(`${number} is divisible by 4`);
+    
+  }
+  else{
+    console.log(`${number} is not divisible by 4`);
+  }
+}
+else{
+  console.log(`${number} is an odd number`);
+}
