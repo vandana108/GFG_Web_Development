09eab6ea65b5c1e0046ba842e2a4d2d6b600f47a
vandana108 <thevandanarai108@gmail.com>
2025-01-18 14:20:34 +0530

    Nullish Coalscing

---
+++ Javascript/Basics/Nullish-Coalscing.js
@@ -0,0 +1,19 @@
+// const a =0;
+// console.log(a ?? 1);
+
+// let x = 12;
+// let y;
+// console.log(x+(X||y));
+
+const str = "vandana";
+console.log(str ?? "HidenGeeks");
+
+// if variable is null or undefined
+const str1 = undefined;
+console.log(str1 ?? "HidenGeeks")
+
+const str2 = null;
+console.log(str2 ?? "Nothing is here")
+
+const str3 = null;
+console.log(str3 ?? "incase of empty space");
