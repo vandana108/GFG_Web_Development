90a8a3f6ee9368e874d7fb3db51956924eb89bfc
vandana108 <thevandanarai108@gmail.com>
2025-01-18 15:44:26 +0530

    for-loop

---
+++ Javascript/Basics/for-loop.js
@@ -0,0 +1,13 @@
+//for loop
+for(let i=0;i<10 ; i++)
+{
+  console.log("Hello World!!....");
+
+}
+console.log("Loop Execution Done!!!")
+
+const userName = "Web Development";
+for(let i=0;i<userName.length;i++)
+{
+  console.log(userName[i]);
+}
