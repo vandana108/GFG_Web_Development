f996e52425bb89eec40056aad1ae37f1ac0e0275
vandana108 <thevandanarai108@gmail.com>
2025-01-27 16:23:59 +0530

    pop and slice method

---
+++ Javascript/Basics/Array/Array-Method-pop and splice.js
@@ -0,0 +1,14 @@
+
+let courses = ["html", "CSS", "JS", "React", "Node", "Python", "SQL"];
+console.log("Courses-: ", courses);
+let removedItem = courses.pop()
+console.log(courses);
+
+//slice, splice
+
+let new_Courses = ["html", "CSS", "Jquery", "OS", "Cyber security"];
+let value = new_Courses.slice(1,3);
+console.log(value);
+
+
+let userName = "prakash";
