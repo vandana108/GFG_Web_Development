470eefa97cfc15e9dd9d57a79e1263f2416432a6
vandana108 <thevandanarai108@gmail.com>
2025-01-27 20:25:23 +0530

    split and join method

--- Javascript/Basics/Array/sort.js
+++ Javascript/Basics/Array/sort.js
@@ -1,14 +1,23 @@
-const fruits = ["apple", "Pineapple", "cherry", "banana", "Mango" ];
- const c = fruits.sort();
-console.log(c);
+// const fruits = ["apple", "Pineapple", "cherry", "banana", "Mango" ];
+//  const c = fruits.sort();
+// console.log(c);
 
-const numbers = [1, 9, 3, 8, 5, 31,27, 20, 11];
+// const numbers = [1, 9, 3, 8, 5, 31,27, 20, 11];
 
-function sortInAscendingOrder(a, b){
-  return a-b;
-}
-function sortInDescendingOrder(a, b){
-  return b-a;
-}
-numbers.sort(sortInDescendingOrder);
-console.log(numbers);
+// function sortInAscendingOrder(a, b){
+//   return a-b;
+// }
+// function sortInDescendingOrder(a, b){
+//   return b-a;
+// }
+// numbers.sort(sortInDescendingOrder);
+// console.log(numbers);
+
+//split() method
+
+const inputStr = "prakash";
+const arrOfChar = inputStr.split("");
+console.log(arrOfChar);
+arrOfChar.reverse();
+const reversedStr = arrOfChar.join("");
+console.log(reversedStr);
