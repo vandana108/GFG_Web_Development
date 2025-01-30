fdc7dffc4587c28b5f4edb6da6892cc92b3c0206
vandana108 <thevandanarai108@gmail.com>
2025-01-30 16:01:25 +0530

    key value

---
+++ Javascript/Basics/Object/key value and entries.js
@@ -0,0 +1,14 @@
+
+const obj ={
+  name:"Ravi",
+  age: 26,
+  country: "Germany",
+}
+const entries = Object.entries(obj);
+console.log(entries);
+
+const keys = Object.keys(obj);
+console.log(keys);
+
+const values = Object.values(obj);
+console.log(values); 
