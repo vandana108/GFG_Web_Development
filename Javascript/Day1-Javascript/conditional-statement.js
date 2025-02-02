f477bf69f5cb85ee77e4d59fb2cc2ce25ab4ac1d
vandana108 <thevandanarai108@gmail.com>
2025-02-02 17:02:07 +0530

    Ternary Operator

--- Javascript/Day1-Javascript/conditional-statement.js
+++ Javascript/Day1-Javascript/conditional-statement.js
@@ -72,29 +72,46 @@
 
 //Logical Operator
 
-const y =7;
-if(y>3 && y<10){
-    console.log('yes');
+// const y =7;
+// if(y>3 && y<10){
+//     console.log('yes');
 
-}
-else{
-    console.log("no")
-}
+// }
+// else{
+//     console.log("no")
+// }
 //given any number, if the number is divisible by 3 then log foo if it is 
 //  divisible by 5 log bar. if it is divisible by borth 3 adn 5 then log foo bar, 
 // otherwise just log number provided, for any number there should only be on output  
 
-const num =30 ;
+// const num =30 ;
 
-ifif( num%3==0 && num%5==0){
-    console.log('foo bar');
-}
-else if(num%3==0){
-    console.log('foo');
-}
-else if(  num%5==0){
-    console.log('bar');
-}
-else{
-    console.log(number);
-}
+// if( num%3==0 && num%5==0){
+//     console.log('foo bar');
+// }
+// else if(num%3==0){
+//     console.log('foo');
+// }
+// else if(  num%5==0){
+//     console.log('bar');
+// }
+// else{
+//     console.log(number);
+// }
+
+
+
+//Ternary Operator-- short hand for if-else condition
+// condition ? execute the true case: execute the false case
+
+// const isLoggedIn = true;
+//const message = isLoggedIn ? 'logged in' : 'logged out'
+
+// console.log('message')
+
+// const x=3;
+// const y= '2';
+// x==y ? console.log('yes'): x>y ? console.log('x is greater than y'): console.log('y is greater than x')
+
+const p= 21;
+(p%3==0 && p%5 ===0)?console.log('foo bar'):(p%5 ==0)?console.log('bar'):(p%3===0)?console.log('foo'):console.log(p);
