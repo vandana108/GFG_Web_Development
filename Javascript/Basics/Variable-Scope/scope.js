edf366a1a595b95966d12094c4c1e98147ef45f1
vandana108 <thevandanarai108@gmail.com>
2025-02-08 00:41:13 +0530

    Scope and Scope Chaining

--- Javascript/Basics/Variable-Scope/scope.js
+++ Javascript/Basics/Variable-Scope/scope.js
@@ -16,7 +16,9 @@ function localScope()
 
 
 
-//block scope
+
+//var is global scope which we can access
+// variables declared with keyword var are either global scope or function Scoped.
 {
   var a = 10 ;
   var b = 20 ;
@@ -24,3 +26,15 @@ function localScope()
  
  console.log(a);
  console.log(b);
+
+
+
+ //local scope: let and const are local scope here because it will not be accessible outside the block and give an error
+ //Block Scope  - visibility only inside a piece of code generally wrapped by curly braces 
+// let and const declared variables are Block Scope
+//  {
+//   let c=20;
+//   const d=10;
+//  }
+//  console.log(c);
+//  console.log(d);
