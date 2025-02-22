d022fece8ed8af23827e114ed90beb205ac2647c
vandana108 <thevandanarai108@gmail.com>
2025-02-22 21:23:43 +0530

    commit

--- GFG_Lecture/Lecture-10/DOM.js
+++ GFG_Lecture/Lecture-10/DOM.js
@@ -22,3 +22,4 @@ uploadButton.addEventListener('click', () =>{
 })
 
 
+
---
+++ GFG_Lecture/Lecture-10/lec-11(Bubbling).js
@@ -0,0 +1,7 @@
+
+const radioContainer = document.getElementById('radio-btn');
+console.log(radioContainer);
+
+radioContainer.addEventListener('input',()=>{
+  console.log(event);
+})
