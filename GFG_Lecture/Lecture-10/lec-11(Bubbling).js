838dcc205765ac18237a7f8d4e7fe304d66e04a6
vandana108 <thevandanarai108@gmail.com>
2025-02-22 23:23:33 +0530

    difference between querySelector and getElementById

--- GFG_Lecture/Lecture-10/lec-11(Bubbling).js
+++ GFG_Lecture/Lecture-10/lec-11(Bubbling).js
@@ -1,7 +1,21 @@
 
-const radioContainer = document.getElementById('radio-btn');
-console.log(radioContainer);
-
-radioContainer.addEventListener('input',()=>{
-  console.log(event);
+document.getElementById('size-M').addEventListener('click', () => {
+  // const radio = document.querySelector('input[name="radio"]:checked');
+  console.log("Size: M");
 })
+
+document.getElementById('size-L').addEventListener('click', () => {
+  // const radio = document.querySelector('input[name="radio"]:checked');
+  console.log("Size: L");
+})
+document.getElementById('size-XL').addEventListener('click', () => {
+  // const radio = document.querySelector('input[name="radio"]:checked');
+  console.log("Size: XL");
+})
+// document.getElementById("radio-btn").addEventListener('click', () =>{
+//   console.log("this is something");
+// })
+
+// radioContainer.addEventListener('input',()=>{
+//   console.log(event);
+// })
