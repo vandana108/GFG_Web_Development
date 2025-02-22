a4c1a023a0e4505ec704246a0e1133405c2da289
vandana108 <thevandanarai108@gmail.com>
2025-02-23 00:00:06 +0530

    commit

--- Javascript/Basics/Classes/inheritance.js
+++ Javascript/Basics/Classes/inheritance.js
@@ -9,18 +9,21 @@ class Laptop{
 
   }
   displayMessage(){
-    console.log(`Laptop Spec:: RAM${this.ram} - Processor ${this.processor} - ${this.generation} Generation `)
+    console.log(`Laptop Spec:: RAM${this.ram} - Processor ${this.processor} - ${this.generation} Generation - modelName${this.modelName} -price ${this.price}`);
   }
 
 }
 //child or subclass
 class Dell extends Laptop{
-  constructor(ram, processor, generation){
+  constructor(ram, processor, generation, modelName, price){
     super(ram, processor, generation); // super means access to parent class
+    this.modelName = modelName;
+    this.price = price;
   }
 }
-const dell = new Dell("8GB", "intel", "i5");
+const dell = new Dell("8GB", "intel", "i5", "Dell Inspiron", 50000);
 console.log(dell);
+dell.displayMessage();
 class Lenovo{
 
 }
