24eb7660ef48ea51918a5e9b4d2a6432ec66b385
vandana108 <thevandanarai108@gmail.com>
2025-03-01 17:39:04 +0530

    Asynchronous Javascript

---
+++ Javascript/Basics/Async.js
@@ -0,0 +1,61 @@
+//Javascript is a single threaded synchronous language
+// setTimeout(()=> {
+//   console.log('hey ffrom timeout');
+// }, 4000);
+
+// console.log('start')
+// setTimeout(()=> {
+//   console.log('hey ffrom timeout');
+// }, 3000);
+
+// setTimeout(()=> {
+//   console.log('bye bye');
+// }, 4000);
+
+// var x= 3;
+// var y = 7;
+// function calculate(a, b){
+//   return a + b;
+// }
+// setTimeout(()=> {
+//   console.log('zero second');
+// },5000);
+//  console.log('start')
+
+//  setTimeout(()=> {
+//   console.log('hey from timeout');
+// }, 3000);
+
+// setTimeout(()=> {
+//   console.log('byye bye');
+// }, 2000);
+
+// console.log('end')
+
+
+
+var x= 3;
+var y = 7;
+function calculate(a, b){
+  return a + b;
+}
+setTimeout(()=> {
+  console.log('zero second');
+},5000);
+ console.log('start')
+
+ setTimeout(()=> {
+  console.log('hey from timeout');
+}, 3000);
+
+setTimeout(()=> {
+  console.log('byye bye');
+}, 2000);
+
+console.log('end');
+
+
+
+setInterval( function(){
+  console.log('hello world');
+}, 4000)
