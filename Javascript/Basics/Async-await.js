22b13bd3f7a6bcf6c2eee3c6c53a1ab600da1b14
vandana108 <thevandanarai108@gmail.com>
2025-03-02 21:56:57 +0530

    Async

---
+++ Javascript/Basics/Async-await.js
@@ -0,0 +1,27 @@
+// async function calculateSum(){
+//   return 1;
+// }
+// console.log(calculateSum());
+
+// async function calculateSum(){
+//   const response = await fetch('https://dummyjson.com/products')
+  
+//   const data = await response.json();
+//   console.log(data)
+// }
+// console.log(calculateSum());
+
+let x;
+if(x===3)
+  console.log('three')
+
+try{
+  if(x===3){
+    console.log('three')
+  }
+  
+}catch(err){
+  console.log(err, 'something is wrong')
+}
+
+console.log('endd')
