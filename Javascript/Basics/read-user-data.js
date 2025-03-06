55939f4e11aa19a40028527f374490ecd65a5e0d
vandana108 <thevandanarai108@gmail.com>
2025-03-06 23:56:42 +0530

    commit

--- Javascript/Basics/read-user-data.js
+++ Javascript/Basics/read-user-data.js
@@ -14,3 +14,9 @@ const userAge = readlineSync.question("May I ask your Age ? ")
 const yearOfBirth= 2025-Number(userAge)
 console.log(`Yor were born in the year fo ${yearOfBirth}`)
 // node Javascript\Basics\read-user-data 
+// Running the Code
+// To run this code, open your terminal, navigate to the directory containing your script, and use the following command:
+
+// node your-script-file.js
+// Replace your-script-file.js with the name of your JavaScript file.
+// 
