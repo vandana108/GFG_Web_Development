860f64c4c632ddc467344482044be69d34e987b8
vandana108 <thevandanarai108@gmail.com>
2025-04-17 09:42:54 +0530

    commit

--- OOPs in Javascript/script.js
+++ OOPs in Javascript/script.js
@@ -1,8 +1,25 @@
-const user = {
-   fistName: 'Anurag',
-   lastName: 'Singh',
-   age: 25,
+// const user = {
+//    fistName: 'Anurag',
+//    lastName: 'Singh',
+//    age: 25,
+// }
+// function getAgeYear(age){
+//   return new Date().getFullYear()-user.
+// }
+
+const user1 = {
+  fistName: 'Anurag',
+  lastName: 'Singh',
+  age: 25,
+  getAgeYear: function(){
+    return new Date.getAgeYear()-user.age
+  }
 }
-function getAgeYear(age){
-  return new Date().getFullYear()-age
+
+const user2 = {
+  firstName: 'Gautam', 
+  age:29, 
+  getAgeYear: function(){
+    return new Date().getFullYear()-user2.age
+  }
 }
