dcde35802d7ba71b1026560107553c60334eebcd
vandana108 <thevandanarai108@gmail.com>
2025-04-19 11:24:02 +0530

    Inheritance

---
+++ Javascript/Basics/class/inheritance.js
@@ -0,0 +1,39 @@
+
+
+//parent class
+class Laptop{
+  constructor(ram, processor, generation){
+    this.ram= ram,
+    this.processor= processor,
+    this.generation = generation
+  }
+  displayMessage(){
+    console.log(`Laptop space: ${this.ram} + processor: ${this.processor} + generation: ${this.generation}`);
+
+  }
+}
+//child class
+class Dell extends Laptop{
+  constructor(ram, processor, generation){
+    super(ram, processor, generation);//Call the parent class's constructor
+  }
+}
+const dell = new Dell('8GB', "intell", "i5");
+console.log(dell);
+
+
+//child class
+class Lenovo extends Laptop{
+  constructor(ram, processor, generation, modelName){
+    super(ram, processor, generation),//Call the parent class's constructor
+    this.modelName = modelName
+    
+  }
+  location(){
+    console.log(`Lenovo's working location is Mumbai and it provides this ${this.modelName} model`)
+  }
+
+}
+const lenovo = new Lenovo('16GB', 'intell', 'i7', 'versonLen7');
+console.log(lenovo);
+lenovo.location();
